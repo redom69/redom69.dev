bc4e74522d21e23e62c9b58ee0c2953d33bbeef8
Daniel Hernandez Puerto <sirdanielsan64@gmail.com>
2024-09-14 15:27:23 +0200

    Header casi funcional falta poner bien funcionalidad de newsletter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+});
